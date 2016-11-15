d824223effc76063625efc8e59788a3fafa89172
Pimp Daddy <student@jmoran2.local>
2016-11-15 11:10:57 -0600

    Initial Commit

---
+++ TictacFLow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TictacFLow
+//
+//  Created by jmoran2 on 11/15/16.
+//  Copyright © 2016 Big J's big booty buddys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
